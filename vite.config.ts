a7d070f5b784dc9191536871378be62e6c320072
leinadalrus <daniel.david.surla@gmail.com>
2025-04-03 13:16:47 +0800

    added localforage cordova sqlite driver for ionic framework.

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,30 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
 
+// Cordova SQLite
+import { Drivers, Storage } from '@ionic/storage'
+import CordovaSQliteDriver from 'localforage-cordovasqlitedriver'
+
+const store = new Storage({
+    driverOrder: [
+        CordovaSQliteDriver._driver,
+        Drivers.IndexedDB,
+        Drivers.LocalStorage
+    ]
+})
+
+await store.defineDriver(CordovaSQliteDriver)
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    legacy()
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [vue(), legacy()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src')
+        }
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom'
-  }
+    test: {
+        globals: true,
+        environment: 'jsdom'
+    }
 })
